cf1c464b31b71fbd55147827ee40c0df526bebe9
demo-user <sdalitaf@gmail.com>
2025-01-15 21:38:03 +0530

    2nd commit

---
+++ providers.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_version = "~> 1.8.4"
+
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 3.21"
+    }
+  }
+
+  backend "s3" {
+    bucket = "demo512"
+    key    = "terraform/snapshot/state.tfstate"
+    region = "us-east-1"
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+
+provider "aws" {
+  alias   = "aws_lab"
+  profile = "default"
+  region  = "us-east-1"
+}
